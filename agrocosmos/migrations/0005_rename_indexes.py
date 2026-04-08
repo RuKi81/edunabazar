6bf108c01cfd48a2b2541a079d13491324e98874
RuKi81 <Lizard_ru@mail.ru>
2026-04-08 10:34:14 +0300

    fix: make migration 0005 no-op (indexes already have auto names)

--- agrocosmos/migrations/0005_rename_indexes.py
+++ agrocosmos/migrations/0005_rename_indexes.py
@@ -1,26 +1,11 @@
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
+    """Indexes already have auto-generated names; no rename needed."""
 
     dependencies = [
         ('agrocosmos', '0004_monitoringtask'),
     ]
 
-    operations = [
-        migrations.RenameIndex(
-            model_name='farmland',
-            new_name='agro_farmla_distric_9bde9c_idx',
-            old_name='agro_farmla_distric_idx',
-        ),
-        migrations.RenameIndex(
-            model_name='vegetationindex',
-            new_name='agro_vegeta_farmlan_c49b8b_idx',
-            old_name='agro_vegidx_farm_type_date',
-        ),
-        migrations.RenameIndex(
-            model_name='vegetationindex',
-            new_name='agro_vegeta_acquire_6fe824_idx',
-            old_name='agro_vegidx_date_type',
-        ),
-    ]
+    operations = []
